46ac0ed75e75feb5109313e4dec6b9e954830dc3
Tsvetelina Abadjieva <c.abadjieva@code-nest.com>
2018-05-15 09:59:33 +0300

    Video module

---
+++ custom_brightcove/templates/brightcove-video.tpl.php
@@ -0,0 +1,10 @@
+<iframe id="videoPlayer"
+  width  = <?php print $width;?>
+  height = <?php print $height;?>
+  src = "https://players.brightcove.net/<?php print $account_id;?>/default_default/index.html?videoId=<?php print $video_id;?>"
+  frameborder="0"
+  autoplay = "0"
+  controls = "1"
+  allow="autoplay; encrypted-media"
+  allowfullscreen = 1>
+</iframe>
